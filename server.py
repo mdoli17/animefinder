6292aa8b6ec94592f3edb8bbfc5192e5861aef0f
Mate <mdoli17@freeuni.edu.ge>
2021-01-07 17:45:04 +0400

    json parsing, fetching data and getting user input

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env python3
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test
-import sys
+#!/usr/bin/env python
+
+# Usage: python cors_http_server.py <port>
+
+try:
+    # try to use Python 3
+    from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
+    import sys
+    def test (*args):
+        test_orig(*args, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+except ImportError: # fall back to Python 2
+    from BaseHTTPServer import HTTPServer, test
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
@@ -8,4 +18,4 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+    test(CORSRequestHandler, HTTPServer)
