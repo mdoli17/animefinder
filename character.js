d4547af5d96f6a977c46c842b1f811014bafe8f4
MateDoli <mate.dolidze1@gmail.com>
2021-02-14 23:10:47 +0400

    homocide

--- character.js
+++ character.js
@@ -8,6 +8,11 @@ function constructor() {
         var character = JSON.parse(sessionStorage.getItem("info"));
 
         document.getElementById("CharacterImage").src = characterFolder + character.name + ".png";
+        document.getElementById("Name").innerHTML = "Name: " + character.name;
+        document.getElementById("Gender").innerHTML = "Gender: " + character.gender;
+        document.getElementById("Age").innerHTML = "Age: " + character.age;
+        document.getElementById("EyeColor").innerHTML = "EyeColor: " + character.eye_color
+        document.getElementById("HairColor").innerHTML = "HairColor: " + character.hair_color
     }, 1000);
 }
 
